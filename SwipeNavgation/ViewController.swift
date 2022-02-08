8e533f38b4afccdf54778d2f2529be38bde3d0dc
d.martynov <d.martynov@globus-ltd.com>
2022-02-08 18:04:01 +0300

    Initial Commit

---
+++ SwipeNavgation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeNavgation
+//
+//  Created by Dmitry Martynov on 08.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
